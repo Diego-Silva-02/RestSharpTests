07bec0f373bdb22ef37121cfeb95f8cdb6c0515c
Diego Silva <diegosilva22a@gmail.com>
2021-09-14 21:51:17 -0300

    Add project files.

---
+++ Models/JsonObjectModel.cs
@@ -0,0 +1,16 @@
+﻿namespace RestSharpTests.Models
+{
+    class JsonObjectModel
+    {
+        public int UserId { get; set; }
+        public string Body { get; set; }
+        public string Title { get; set; }
+
+        public JsonObjectModel(int userId, string body, string title)
+        {
+            UserId = userId;
+            Body = body;
+            Title = title;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using RestSharp;
+using RestSharpTests.Models;
+
+namespace RestSharpTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseUrl = "https://jsonplaceholder.typicode.com/posts";
+
+            // Sets the BaseUrl for requests made by the client instance
+            RestClient client = new RestClient(baseUrl);
+
+            // Data used to make requests
+            RestRequest request = new RestRequest();
+
+            // This method ask the user to enter the data
+            JsonObjectModel objectModel = GenerateJsonModel();
+
+            // Give an object in the request body
+            request.AddJsonBody(objectModel);
+
+            // Execute the request using HTTP POST method
+            IRestResponse response = client.Post(request);
+
+            Console.WriteLine(response.StatusCode + Environment.NewLine + response.Content.ToString());
+
+            Console.Read();
+        }
+
+        static JsonObjectModel GenerateJsonModel()
+        {
+            Console.WriteLine("Enter the User Id:");
+            int userId = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Body:");
+            string body = Console.ReadLine();
+
+            Console.WriteLine("Enter the title:");
+            string title = Console.ReadLine();
+
+            JsonObjectModel objectModel = new JsonObjectModel(userId, body, title);
+
+            return objectModel;
+        }
+    }
+}
